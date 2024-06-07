5b9083149acf1c3f235fb4c175cc037b3df7e6d2
ahme1kash <kashifrichy@gmail.com>
2024-06-07 22:07:02 +0530

    Fun Proj -- CSS Hovering

---
+++ script.js
@@ -0,0 +1,9 @@
+// console.log("hello");
+
+// function custom(event) {
+//     var el = document.getElementById("hov");
+//     el.style.top = event.clientY + "px";
+//     el.style.left = event.clientX + "px";
+// }
+
+// document.querySelector(".pic").addEventListener('mousemove', custom);
